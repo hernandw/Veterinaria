74042b74a50e0671b2a4e82576971bee4d57f352
Williams Hernandez <hernandw@gmail.com>
2022-04-19 12:05:34 -0400

    inicio proyecto y creación de componentes

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Header from "./components/Header";
+
+import "./App.css";
+import Formulario from "./components/Formulario";
+import ListadoPacientes from "./components/ListadoPacientes";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Formulario />
+      <ListadoPacientes />
+    </div>
+  );
+}
+
+export default App;
